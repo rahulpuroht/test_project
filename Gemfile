08359f563712699e3239f79d7a7516984015af9c
Rohan Khanna <rohan.khanna@mobiloittegroup.com>
2017-11-18 19:25:14 +0530

    capistrano setup for jenkins

--- Gemfile
+++ Gemfile
@@ -39,6 +39,10 @@ group :development, :test do
   # Adds support for Capybara system testing and selenium driver
   gem 'capybara', '~> 2.13'
   gem 'selenium-webdriver'
+   gem 'capistrano-rails'
+  gem 'capistrano-bundler'
+  gem 'capistrano-rvm'
+  gem 'capistrano-ssh-doctor'
 end
 
 group :development do
---
+++ config/deploy.rb
@@ -0,0 +1,91 @@
+# config valid only for current version of Capistrano
+lock "3.8.1"
+
+# set :application, "my_app_name"
+# set :repo_url, "git@example.com:me/my_repo.git"
+
+# Default branch is :master
+# ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
+
+# Default deploy_to directory is /var/www/my_app_name
+# set :deploy_to, "/var/www/my_app_name"
+
+# Default value for :format is :airbrussh.
+# set :format, :airbrussh
+
+# You can configure the Airbrussh format using :format_options.
+# These are the defaults.
+# set :format_options, command_output: true, log_file: "log/capistrano.log", color: :auto, truncate: :auto
+
+# Default value for :pty is false
+# set :pty, true
+
+# Default value for :linked_files is []
+# append :linked_files, "config/database.yml", "config/secrets.yml"
+
+# Default value for linked_dirs is []
+# append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
+
+# Default value for default_env is {}
+# set :default_env, { path: "/opt/ruby/bin:$PATH" }
+
+# Default value for keep_releases is 5
+# set :keep_releases, 5
+
+# config valid only for current version of Capistrano
+set :application, 'test'
+
+# Default value for :scm is :git
+set :scm, :git
+# set :repo_url, 'git@bitbucket.org:mobiloitteinc/mpossoftwareshopkeeperwithprinteranddrawerintegration-15082381.git'
+set :repo_url, 'https://github.com/rahulpuroht/test_project.git'
+
+# Default deploy_to directory is /var/www/my_app_name
+set :deploy_to, '/home/ubuntu/testing_jenkins'
+
+# Default branch is :master
+ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
+
+# Default value for :format is :pretty
+set :format, :pretty
+
+# Default value for :log_level is :debug
+# set :log_level, :debug
+
+# Default value for :pty is false
+set :pty, true
+
+# Default value for :linked_files is []
+# set :linked_files, fetch(:linked_files, []).push('config/application.yml','config/database.yml', 'config/secrets.yml', 'config/cloudinary.yml')
+
+# Default value for linked_dirs is []
+# set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
+
+# Default value for default_env is {}
+# set :default_env, { path: "/opt/ruby/bin:$PATH" }
+
+# Default value for keep_releases is 5
+# set :keep_releases, 5
+
+namespace :deploy do
+
+  desc 'Restart application'
+  task :restart do
+    on roles(:app), in: :sequence, wait: 5 do
+      execute 'sudo service nginx restart'
+    end
+  end
+
+  after :publishing, :restart
+
+  # after :restart, :clear_cache do
+  #   on roles(:web), in: :groups, limit: 3, wait: 10 do
+  #     # Here we can do anything such as:
+  #     within release_path do
+  #       execute :rake, 'cache:clear'
+  #     end
+  #   end
+  # end
+
+end
+
