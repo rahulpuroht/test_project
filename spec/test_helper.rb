73484633d147d5219c9e40015d542b417c62983f
Rohan Khanna <rohan.khanna@mobiloittegroup.com>
2017-11-22 16:50:55 +0530

    pass test case and failed test case added

--- spec/test_helper.rb
+++ spec/test_helper.rb
@@ -3,7 +3,11 @@ require 'rails_helper'
 RSpec.describe "Welcome", type: :request do
   
 
-  it "should have the right title" do
+  	it "should have the right title" do
+      get '/'
+    end
+
+    it "should have the right url" do
       get '/home'
     end
 
